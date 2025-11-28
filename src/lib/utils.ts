9961c17d407ef70a6cfc1bc1154b4e3e8f1f0994
greg <gregjc@protonmail..com>
2025-11-28 16:19:47 +1300

    Timezones

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import timezones from "@/timezones.json";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+type Timezone = typeof timezones[0];
+
+export const getTimezonesByString = (text: string): Timezone[] => {
+  const normalizedString = text.trim().toLowerCase();
+
+  return timezones.filter((tz: Timezone) => 
+    tz.value.toLowerCase().includes(normalizedString)
+    || tz.abbr.toLowerCase().includes(normalizedString)
+    || tz.text.toLowerCase().includes(normalizedString)
+    || tz.utc.find(utc => utc.toLowerCase().includes(normalizedString))
+  );
+};
