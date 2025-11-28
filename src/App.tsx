a08dc62b271d996a80cf54a1875945a2cd6b215c
greg <gregjc@protonmail..com>
2025-11-28 15:51:21 +1300

    Tidy up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import './App.css';
 import DatePicker from './components/DatePicker';
 import TimePicker from './components/TimePicker';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
