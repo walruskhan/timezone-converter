4122aa2388c2cb0e45710df3020044339076488c
greg <gregjc@protonmail..com>
2025-11-28 15:50:03 +1300

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import './App.css'
-import DatePicker from './components/DatePicker'
-import TimePicker from './components/TimePicker'
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import './App.css';
+import DatePicker from './components/DatePicker';
+import TimePicker from './components/TimePicker';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
-      <DatePicker placeholder='Date' />
-      <TimePicker onDateChange={v => console.log(v)} />
+      <DatePicker placeholder="Date" />
+      <TimePicker onDateChange={(v) => console.log(v)} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,51 +1,64 @@
-'use client'
+'use client';
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
-import { CalendarIcon, ChevronDownIcon } from 'lucide-react'
+import { CalendarIcon, ChevronDownIcon } from 'lucide-react';
 
-import { Button } from '@/components/ui/button'
-import { Calendar } from '@/components/ui/calendar'
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 
 export interface DatePickerWithIcon {
-  date?: Date,
-  placeholder?: string
+  date?: Date;
+  placeholder?: string;
 }
 
-const DatePickerWithIconDemo = ({ date: initialDateValue, placeholder = "Pick a Date" }: DatePickerWithIcon) => {
-  const [open, setOpen] = useState(false)
-  const [date, setDate] = useState<Date | undefined>(initialDateValue ?? new Date())
+const DatePickerWithIconDemo = ({
+  date: initialDateValue,
+  placeholder = 'Pick a Date',
+}: DatePickerWithIcon) => {
+  const [open, setOpen] = useState(false);
+  const [date, setDate] = useState<Date | undefined>(
+    initialDateValue ?? new Date()
+  );
 
   useEffect(() => {
     setDate(initialDateValue);
-  }, [initialDateValue])
+  }, [initialDateValue]);
 
   return (
-    <div className='w-full max-w-xs space-y-2'>
+    <div className="w-full max-w-xs space-y-2">
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
-          <Button variant='outline' id='date' className='w-full justify-between font-normal'>
-            <span className='flex items-center'>
-              <CalendarIcon className='mr-2' />
+          <Button
+            variant="outline"
+            id="date"
+            className="w-full justify-between font-normal"
+          >
+            <span className="flex items-center">
+              <CalendarIcon className="mr-2" />
               {date ? date.toLocaleDateString() : placeholder}
             </span>
             <ChevronDownIcon />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className='w-auto overflow-hidden p-0' align='start'>
+        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
           <Calendar
-            mode='single'
+            mode="single"
             selected={date}
-            onSelect={date => {
-              setDate(date)
-              setOpen(false)
+            onSelect={(date) => {
+              setDate(date);
+              setOpen(false);
             }}
           />
         </PopoverContent>
       </Popover>
     </div>
-  )
-}
+  );
+};
 
-export default DatePickerWithIconDemo
+export default DatePickerWithIconDemo;
