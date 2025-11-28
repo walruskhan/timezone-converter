b5c00ef2b5a193c17fc92218ddfa94ef8626c1b0
greg <gregjc@protonmail..com>
2025-11-28 15:16:41 +1300

    Setup vite+react+shadcn

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
